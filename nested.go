18d7a675120b3075a2f4fc93a2fd9444404d6740
Pawan Kumar <pawan@tykeinvest.com>
2023-04-12 17:38:43 +0530

    initial commit

---
+++ nested.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"errors"
+	"strconv"
+	"strings"
+)
+
+// Get takes data map[string]any and keys ...string and it searches for value
+// nested within data using keys. for example data["key1"]["key2"]["key3"].
+//
+// if any previous key returns array then it tries to convert current key into integer for indexing.
+//
+// if any previous key returns non-indexable (number, string, boolean) and there are remaining keys to search
+// then it will return error and returns closest value it can find
+func Get(data map[string]any, keys ...string) (any, error) {
+	value := any(data)
+	for _, key := range keys {
+		if isObject(value) {
+			value = value.(map[string]any)[key]
+		} else if isArray(value) {
+			index, err := strconv.ParseInt(key, 10, 64)
+			if err != nil {
+				return value, errors.New("got array but key can not be converted to int")
+			}
+			if index >= int64(len(value.([]any))) {
+				return value, errors.New("index out of bound")
+			}
+			value = value.([]any)[index].(any)
+		} else {
+			// there is a key that it need to get but the prev value is neither map nor array
+			return value, errors.New("previous value is not (array or map)")
+		}
+	}
+	return value, nil
+}
+
+// Gets() calls Get() after spliting key on dot (.)
+func Gets(data map[string]any, key string) (any, error) {
+	return Get(data, strings.Split(key, ".")...)
+}
+
+func isObject(d any) bool {
+	if _, ok := d.(map[string]any); !ok {
+		return false
+	} else {
+		return true
+	}
+}
+
+func isArray(d any) bool {
+	if _, ok := d.([]any); !ok {
+		return false
+	}
+	return true
+}
---
+++ nested_test.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+)
+
+func TestGet(t *testing.T) {
+	raw := []byte(`{
+    "k":[false, 3, 4, true, "string"],
+    "d": {
+      "e": [ { "name": "mango" }, { "name": "banana" } ],
+      "f": {"j": false}
+    },
+    "m": {
+      "0": "zero",
+      "1": {
+        "2": "two",
+        "3": [1,2,43]
+      }
+    }
+  }`)
+
+	var stuff map[string]any
+	err := json.Unmarshal(raw, &stuff)
+	if err != nil {
+		panic("can not parse test json")
+	}
+
+	value, err := Get(stuff, "k", "1")
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+	}
+
+	if value.(float64) != 3 {
+		t.Fail()
+	}
+
+	value, err = Get(stuff, "k", "d", "a")
+	if err == nil {
+		t.Fail()
+	}
+
+	value, err = Get(stuff, "d", "e", "0", "name")
+	if err != nil {
+		t.Fail()
+	}
+
+	if value.(string) != "mango" {
+		t.Fail()
+	}
+
+	value, err1 := Get(stuff, "d", "e", "0", "name", "cow")
+	if err1 == nil {
+		t.Fail()
+	}
+
+	value, err = Get(stuff, "d", "e", "100", "name")
+	if err == nil {
+		t.Fail()
+	}
+	value, err = Get(stuff, "m", "1", "2")
+	if err != nil {
+		t.Fail()
+	}
+
+	if value.(string) != "two" {
+		t.Fail()
+	}
+
+	//--
+
+	value, err = Get(stuff, "m", "1", "3", "2")
+	if err != nil {
+		t.Fail()
+	}
+
+	if value.(float64) != 43 {
+		t.Fail()
+	}
+}
